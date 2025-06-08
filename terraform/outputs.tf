200e2887c601ec63ba08e946dfbbe7c6b2a5abe7
Akhilesh Kola <111245319+Kola6@users.noreply.github.com>
2025-06-08 13:02:46 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,8 @@
-# Placeholder content for outputs.tf
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
