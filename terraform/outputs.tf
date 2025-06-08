cc6df4291cca80abf02ca4be690323070d374799
Akhilesh Kola <111245319+Kola6@users.noreply.github.com>
2025-06-08 13:30:02 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,21 @@
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+output "db_fqdn" {
+  value = azurerm_postgresql_flexible_server.db.fqdn
+}
+
+output "dns_zone_name" {
+  value = azurerm_private_dns_zone.internal_dns.name
+}
+
+output "aks_kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
 
